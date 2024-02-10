8d9e2d88504b34e8388bb1650044a959e1a2fbaa
anpan2val <anpan2val@wevals.com>
2024-02-10 23:48:30 +0900

    add material base lib

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -2,21 +2,35 @@ import React from 'react'
 import {createRoot} from 'react-dom/client'
 import {CssBaseline} from '@mui/material'
 import ApexChart from "./ApexChart";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Button from '@mui/material/Button';
 
 // root element
 const root = createRoot(
     document.getElementById('app') as HTMLElement
 );
 
+const theme =
+    createTheme({
+        pallet: {
+            primary: {
+                main: '#556cd6',
+            },
+            // other custimeze..
+        },
+    });
+
 const App = () => {
 
     return (
         <>
-            <CssBaseline />
-            <ApexChart />
+            <ThemeProvider theme={theme}>
+                <Button variant="contained" color="primary">
+                    Hello World
+                </Button>
+            </ThemeProvider>
         </>
     );
-
 };
 
 root.render(<App />)
