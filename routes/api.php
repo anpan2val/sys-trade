88386c24f9c0fc20eb839d095eb81037343e5516
anpan2val <anpan2val@wevals.com>
2024-02-12 21:26:33 +0900

    add cmc api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/cmc', function (Request $request) {
+    return response()->json(
+        [
+            'cmc' => 'sample data'
+        ]
+    );
+});
+
