679f7a28cd19640c71b2705629f20bead0604733
anpan2val <anpan2val@wevals.com>
2024-02-15 23:34:01 +0900

    add dummy api endpoint

--- app/Http/Controllers/CmcApiController.php
+++ app/Http/Controllers/CmcApiController.php
@@ -1,10 +1,26 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Http;
 
 class CmcApiController extends Controller
 {
+    public function dummy()
+    {
+        // dummy data for debug
+        return array(
+            'data' => [
+                'price' => '51698.999513739',
+                'volume_24h' => '42135855135.304',
+                'volume_change_24h' => '3.4689',
+                'percent_change_1h' => '0.30816671',
+                'percent_change_24h' => '3.71978656',
+                'percent_change_7d' => '19.89281289',
+            ],
+        );
+    }
+
     public function index()
     {
         $res = Http::withHeaders([
@@ -14,12 +30,12 @@ class CmcApiController extends Controller
         return response()->json(
             [
                 'data' =>
-                ['price' => $res['data'][0]['quote']['USD']['price']],
-                ['volume_24h' => $res['data'][0]['quote']['USD']['volume_24h']],
-                ['volume_change_24h' => $res['data'][0]['quote']['USD']['volume_change_24h']],
-                ['percent_change_1h' => $res['data'][0]['quote']['USD']['percent_change_1h']],
-                ['percent_change_24h' => $res['data'][0]['quote']['USD']['percent_change_24h']],
-                ['percent_change_7d' => $res['data'][0]['quote']['USD']['percent_change_7d']],
+                    ['price' => $res['data'][0]['quote']['USD']['price']], // 51698.999513739
+                ['volume_24h' => $res['data'][0]['quote']['USD']['volume_24h']], // 42135855135.304
+                ['volume_change_24h' => $res['data'][0]['quote']['USD']['volume_change_24h']], // 3.4689
+                ['percent_change_1h' => $res['data'][0]['quote']['USD']['percent_change_1h']], // 0.30816671
+                ['percent_change_24h' => $res['data'][0]['quote']['USD']['percent_change_24h']], // 3.71978656
+                ['percent_change_7d' => $res['data'][0]['quote']['USD']['percent_change_7d']], // 19.89281289
             ]
         );
     }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/cmc', [CmcApiController::class, 'index']);
+//Route::get('/cmc', [CmcApiController::class, 'index']);
+Route::get('/cmc', [CmcApiController::class, 'dummy']);
