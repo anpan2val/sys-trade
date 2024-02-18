0af90885c79fee5a02b633e0824ad4554f38bac9
anpan2val <anpan2val@wevals.com>
2024-02-18 23:36:57 +0900

    Instead of executing the API every time, we made use of the cache

--- app/Http/Controllers/CmcApiController.php
+++ app/Http/Controllers/CmcApiController.php
@@ -3,22 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 
 class CmcApiController extends Controller
 {
     public function dummy()
     {
-        // dummy data for debug
-        return array(
-            'data' => [
-                'price' => floor(51698.999513739),
-                'volume_24h' => floor(42135855135.304),
-                'volume_change_24h' => floor(3.4689 * 10) / 10,
-                'percent_change_1h' => floor(0.30816671 * 10) / 10,
-                'percent_change_24h' => floor(3.71978656 * 10) / 10,
-                'percent_change_7d' => floor(19.89281289 * 10) / 10,
-            ],
-        );
+        $key = 'cmc_data_' . date('YmdHi');
+
+        $value = Cache::get($key);
+
+        if (!$value) {
+
+            \Log::info('Cache not exist');
+            $value = array(
+                'data' => [
+                    'price' => floor(51698.999513739),
+                    'volume_24h' => floor(42135855135.304),
+                    'volume_change_24h' => floor(3.4689 * 10) / 10,
+                    'percent_change_1h' => floor(0.30816671 * 10) / 10,
+                    'percent_change_24h' => floor(3.71978656 * 10) / 10,
+                    'percent_change_7d' => floor(19.89281289 * 10) / 10,
+                ],
+            );
+
+            $value = json_encode($value, true);
+
+            Cache::set($key, $value, 60);
+        } else {
+
+            \Log::info('Cache exist');
+        }
+
+        return $value;
+
     }
 
     public function index()
