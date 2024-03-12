22238c2277b0a89f62b820214fdd8244b843e6f3
anpan2val <anpan2val@wevals.com>
2024-03-12 23:23:26 +0900

    Add Terraform default

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,35 @@
+resource "aws_vpc" "my_vpc" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    tags = {
+        Name = "my_vpc"
+    }
+}
+
+resource "aws_subnet" "my_subnet" {
+    vpc_id                  = aws_vpc.my_vpc.id
+    cidr_block              = "10.0.1.0/24"
+    map_public_ip_on_launch = true
+    availability_zone       = "us-west-2a"
+}
+
+resource "aws_security_group" "allow_web" {
+    name        = "allow_web"
+    description = "Allow Web inbound traffic"
+    vpc_id      = aws_vpc.my_vpc.id
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
