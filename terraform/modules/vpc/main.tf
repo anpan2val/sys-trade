f96a1f819ed41d47602e41366fadcb3d727ab57d
anpan2val <anpan2val@wevals.com>
2024-03-15 16:16:03 +0900

    change availabilityZone for InvalidParameterValue

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "my_subnet" {
     vpc_id                  = aws_vpc.my_vpc.id
     cidr_block              = "10.0.1.0/24"
     map_public_ip_on_launch = true
-    availability_zone       = "us-west-2a"
+    availability_zone       = "ap-northeast-1a"
 }
 
 resource "aws_security_group" "allow_web" {
